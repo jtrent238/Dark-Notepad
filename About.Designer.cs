888f34c6eef7361fecbdda397a6446010f7a8ab5
Trent Patterson <jtrent238@outlook.com>
2017-09-22 22:27:34 -0400

    Add files via upload

---
+++ About.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace MyNotepad {
+    partial class About {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.controlSimplyGoodCodeLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(267, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dark Notepad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(274, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "A Notepad for all your \"Dark\" needs!";
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonOk.Location = new System.Drawing.Point(542, 207);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 2;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DarkNotepad.Properties.Resources.BigIcon;
+            this.pictureBox1.Location = new System.Drawing.Point(-8, -13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(269, 260);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(274, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(298, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Main code by Luis Perez this version is a fork from his version.";
+            // 
+            // controlSimplyGoodCodeLinkLabel
+            // 
+            this.controlSimplyGoodCodeLinkLabel.AutoSize = true;
+            this.controlSimplyGoodCodeLinkLabel.Location = new System.Drawing.Point(490, 90);
+            this.controlSimplyGoodCodeLinkLabel.Name = "controlSimplyGoodCodeLinkLabel";
+            this.controlSimplyGoodCodeLinkLabel.Size = new System.Drawing.Size(78, 13);
+            this.controlSimplyGoodCodeLinkLabel.TabIndex = 5;
+            this.controlSimplyGoodCodeLinkLabel.TabStop = true;
+            this.controlSimplyGoodCodeLinkLabel.Text = "Notepad Clone";
+            this.controlSimplyGoodCodeLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.controlSimplyGoodCodeLinkLabel_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(274, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Feel free to see his source code on Github.";
+            // 
+            // About
+            // 
+            this.AcceptButton = this.buttonOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonOk;
+            this.ClientSize = new System.Drawing.Size(629, 242);
+            this.Controls.Add(this.controlSimplyGoodCodeLinkLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "About";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About Dark Notepad ";
+            this.Load += new System.EventHandler(this.About_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel controlSimplyGoodCodeLinkLabel;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ About.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace MyNotepad {
+    public partial class About : Form {
+        public About() {
+            InitializeComponent();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e) {
+            Close();
+        }
+
+        private void controlSimplyGoodCodeLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+            System.Diagnostics.Process.Start("https://github.com/luisperezphd/NotepadClone");
+        }
+
+        private void About_Load(object sender, EventArgs e)
+        {
+            //InitializeComponent();
+            //this.Text = String.Format("About {0}", Title);
+            //this.label5.Text = AssemblyProduct;
+            //this.label6.Text = String.Format("Version {0}", AssemblyVersion);
+            //this.label7.Text = AssemblyCopyright;
+            //this.label8.Text = AssemblyCompany;
+            //this.textBoxDescription.Text = AssemblyDescription;
+        }
+    }
+}
